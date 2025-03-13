c17be8a0e7b63eaa861882c5a142207e9a506538
jaimegomez <jaimegfuenmayor@gmail.com>
2025-03-13 12:13:50 +0100

    feat: change test for GlobalExceptionHandlerTest for sonar

--- src/test/java/com/example/starwars/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/com/example/starwars/exception/GlobalExceptionHandlerTest.java
@@ -1,26 +1,30 @@
 package com.example.starwars.exception;
 
 import com.example.starwars.domain.exception.PriceNotFoundExceptionDomain;
+import com.example.starwars.infrastructure.repository.PriceRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@WebMvcTest(GlobalExceptionHandler.class)
 class GlobalExceptionHandlerTest {
 
-    private final GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    PriceRepository priceRepository;
 
     @Test
-    void testHandlePriceNotFound() {
-        // Arrange
-        PriceNotFoundExceptionDomain exception = new PriceNotFoundExceptionDomain("Price not found");
-
-        // Act
-        ResponseEntity<Void> response = globalExceptionHandler.handlePriceNotFound(exception);
-
-        // Assert
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    void testHandlePriceNotFound() throws Exception {
+        // Simular una petición que cause la excepción
+        mockMvc.perform(get("/some-endpoint") // Usa cualquier endpoint de tu aplicación
+                        .requestAttr("exception", new PriceNotFoundExceptionDomain("Price not found")))
+                .andExpect(status().isNotFound());
     }
 }
